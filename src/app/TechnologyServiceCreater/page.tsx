700270bf4fcf9bd478612065c34a2fa2f9816ce4
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-08-28 12:41:06 +0530

    feat(Header): add dynamic props for title, images and description
    Make Header component reusable by accepting props for title, background image, logo image and description. This allows customization for different pages while maintaining consistent styling.

--- src/app/TechnologyServiceCreater/page.tsx
+++ src/app/TechnologyServiceCreater/page.tsx
@@ -4,7 +4,16 @@ import Header from "../../components/common/Header";
 const page = () => {
   return (
     <>
-      <Header />
+      <Header
+        title="Technology Service Creater"
+        bgImage={"/TSCHero.jpg"}
+        logoImage={"/TSCLogo.png"}
+        description="Technology Service Creater (TSC) is a dynamic wing of TWC focused on
+        delivering innovative, scalable, and intelligent digital services
+        that solve real- world problems. We specialize in custom digital
+        solutions, digital platforms, automation, data intelligence, and
+        smart service systems for businesses, institutions, and governments."
+      />
     </>
   );
 };
