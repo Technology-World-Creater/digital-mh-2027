fcb6e1d8cb2c103994f60fdca6433e7b282dad7d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-09-02 16:24:53 +0530

    style(Navbar): increase width of initiatives dropdown menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -89,7 +89,7 @@ const Navbar = () => {
                 </svg>
               </button>
               {isInitiativesDropdownOpen && (
-                <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50">
+                <div className="absolute left-0 mt-2 w-64 bg-white rounded-md shadow-lg py-1 z-50">
                   <ul>
                     <li>
                       <Link
